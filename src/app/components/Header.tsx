760799a28f1f5c6a6bf82f35e3b4f18082ebc054
Ladysm <leidysuarez55@gmail.com>
2024-07-26 23:31:36 -0500

    feat: styling header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { House } from 'lucide-react';
+
 
 const Header = () => {
   return (
-    <div>Menu</div>
+    <header className='flex justify-center items-center w-full'>
+      <div className='flex justify-between gap-8 py-3 px-5 max-w-4xl w-full'>
+      <div className='min-w-32 flex gap-2'>
+        <Image className='w-full' src="/logo.svg" alt='logo' width={90} height={90}/>
+        <div className='flex flex-col items-center justify-center '><h5 className='text-md text-nowrap leading-5'>ECO D SOL</h5>
+        <p className='text-sm leading-4'>Corporación</p></div>
+      </div>
+     <nav className='flex items-center text-md'>
+      <ul className='flex justify-between gap-4'>
+        <li>Inicio</li>
+        <li>Nosotros</li>
+        <li>Servicios</li>
+        <li>Donaciones</li>
+        <li>Eventos</li>
+      </ul>
+     </nav>
+     <Link className='flex items-center bg-yellow  rounded-full px-5 gap-2 text-sm' href="#"><House size={16}  />Contact </Link>
+     </div>
+    </header>
+    
   )
 }
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,18 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    colors: {
+      'blue': '#1fb6ff',
+      'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': 'rgba(238, 200, 67, 0.3)',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+      
+    },
   },
   plugins: [],
 };
