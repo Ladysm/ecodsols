5fbae3018de798147994bde1e44f0c4c37b70936
Sebastian Giraldo <joabgiraldo@gmail.com>
2024-07-28 20:43:14 -0500

    Feat: new section with logic scroll change information

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
 import Image from "next/image";
 import Header from "./components/Header";
 import Banner from "./components/Banner";
+import Team from "./components/Team";
+
 export default function Home() {
   return (
-    <main>
-<Header/>
-<Banner/>
-
+    <main className="flex flex-col gap-10">
+      <Header />
+      <Banner />
+      <Team />
+      <Banner />
+      <Banner />
+      <Banner />
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,12 +10,24 @@ const config: Config = {
     extend: {
       keyframes: {
         bubble: {
-          '0%, 100%': { transform: 'translateY(0)' },
-          '50%': { transform: 'translateY(-10px)' },
+          "0%, 100%": { transform: "translateY(0)" },
+          "50%": { transform: "translateY(-10px)" },
+        },
+
+        fadeInUp: {
+          "0%": { opacity: "0", transform: "translateY(20px)" },
+          "100%": { opacity: "1", transform: "translateY(0)" },
+        },
+        fadeOutDown: {
+          "0%": { opacity: "1", transform: "translateY(0)" },
+          "100%": { opacity: "0", transform: "translateY(20px)" },
         },
       },
+
       animation: {
-        bubble: 'bubble 2s infinite',
+        bubble: "bubble 2s infinite",
+        fadeInUp: "fadeInUp 0.3s ease-out",
+        fadeOutDown: "fadeOutDown 0.3s ease-out",
       },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
@@ -24,20 +36,19 @@ const config: Config = {
       },
     },
     colors: {
-      'blue': '#1fb6ff',
-      'purple': '#7e5bef',
-      'pink': '#ff49db',
-      'orange': '#ff7849',
-      'green': '#13ce66',
-      'yellow': 'rgba(238, 200, 67, 0.3)',
-      'gray-dark': '#273444',
-      'gray': '#8492a6',
-      'gray-light': '#d3dce6',
-      'dark': '#292929',
-      'white': '#FFFFFF',
-      'customGray': '#F5F5F5',
-      'customYellow': '#FFC700',
-      
+      blue: "#1fb6ff",
+      purple: "#7e5bef",
+      pink: "#ff49db",
+      orange: "#ff7849",
+      green: "#13ce66",
+      yellow: "rgba(238, 200, 67, 0.3)",
+      "gray-dark": "#273444",
+      gray: "#8492a6",
+      "gray-light": "#d3dce6",
+      dark: "#292929",
+      white: "#FFFFFF",
+      customGray: "#F5F5F5",
+      customYellow: "#FFC700",
     },
   },
   plugins: [],
