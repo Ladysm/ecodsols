9d5aa9c7bb4383ffdb54b128cc11d1a4aec5c47e
Sebastian Giraldo <joabgiraldo@gmail.com>
2024-07-31 21:17:59 -0500

    Feat: filtered types

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -1,15 +1,12 @@
 import React from 'react'
+import { Services } from '../config/list'
 
-interface card {
-number: string
-titule: string
-description: string
-}
-export const Card = ({ number, titule, description }: card) => {
+export const Card = ({ number, name, type, description }: Services) => {
   return (
     <div className='flex flex-col w-2/3 max-w-2/3 h-1/2 max-h-fit border-2 border-black px-10 py-10 rounded-xl border-b-8 border-black gap-2'>
+      <p>{type}</p>
       <h2 className='text-6xl font-semibold'>{number}</h2>
-      <h3 className='text-2xl font-semibold'>{titule}</h3>
+      <h3 className='text-2xl font-semibold'>{name}</h3>
       <p>{description}</p>
 
     </div>
